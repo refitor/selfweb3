4ad8b9fa7d4a878e38af8aab639536f319ad0084
Jacky <Jacky@refitor.com>
2023-08-25 15:42:12 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
+go mod init selfweb3
+go mod tidy
 
 # build selfweb3.wasm
 cd ./backend/wasm
-# go mod init selfweb3
-# go mod tidy
 rm ../../web3/public/selfweb3.wasm
 GOOS=js GOARCH=wasm go build -ldflags="-w -s" -o ../../web3/public/selfweb3.wasm
 cd ../../
