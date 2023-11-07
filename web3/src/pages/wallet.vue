7cd5728e51488cd5ceb52e945412f028d9829639
refitor <refitor@126.com>
2023-11-08 03:56:57 +0800

    deploy: redeploy goerli contract to 0xC45A26Fc2214C88C2fA6966C92a43f9e940C7254

--- web3/src/pages/wallet.vue
+++ web3/src/pages/wallet.vue
@@ -24,7 +24,7 @@ export default {
             web3: null,
             networkId: '',
             contractAddrMap: {
-                '5': '0xcE25460c82A2dE7D4bBEd1fA98C4a3f27f6362df',
+                '5': '0xC45A26Fc2214C88C2fA6966C92a43f9e940C7254',
                 '1': '0x6AB8c33715F6F4f18bF605F63F0e7b235eeC1027',
 				'5611': '0x7B6E05a55B1756f827F205BF454BF75288904ecF',
                 '421613': '0xec04F8Ee0493f3d763AB1624BB6aAcaCD94Ac4C1'
