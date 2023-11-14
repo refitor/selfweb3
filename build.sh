f991e34ccbc609feffcc87a2601180caaf5952c4
refitor <refitor@126.com>
2023-11-15 04:49:34 +0800

    update build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ go mod tidy
 # build selfweb3.wasm
 cd ./backend/wasm
 rm ../../web3/public/selfweb3.wasm
-GOOS=js GOARCH=wasm go build -ldflags="-w -s" -o ../../web3/public/selfweb3.wasm
+GOOS=js GOARCH=wasm go build -ldflags="-w -s" -o ../../rsweb/selfweb3.wasm
 cd ../../
 
 # # build web
